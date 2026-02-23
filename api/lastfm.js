6e0521ebcfd1a786f78a14a5b310a5f154345402
NimiGames68 <204414320+NimiGames68@users.noreply.github.com>
2026-02-23 21:00:23 +0000

    Update Last.fm API credentials environment variables

--- api/lastfm.js
+++ api/lastfm.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
-  const username = process.env.nimi_games68;
-  const apiKey = process.env.7be9d9fa04a11fc997fdf69756441f02;
+  const username = process.env.LASTFM_USERNAME;
+  const apiKey = process.env.LASTFM_API_KEY;
 
   const response = await fetch(
     `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json&limit=1`
